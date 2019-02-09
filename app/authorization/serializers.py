4400a707dfa1c6c86873fe4dd7432a77ecc19f0f
artek.chumak <artek.chumak@gmail.com>
2019-02-09 13:25:56 +0300

    Add user view.

--- app/authorization/serializers.py
+++ app/authorization/serializers.py
@@ -1,5 +1,4 @@
-from django.contrib.auth import get_user_model
-
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 
 
@@ -11,3 +10,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
+
+    def is_valid(self, raise_exception=False):
+        user = authenticate(username=self.username, password=self.password)
+        if not user:
+            return False
+        return super(UserSerializer, self).is_valid(
+            raise_exception=raise_exception)
--- app/authorization/views.py
+++ app/authorization/views.py
@@ -1,5 +1,41 @@
+from django.contrib.auth import login, logout
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_404_NOT_FOUND,
+)
 from rest_framework.viewsets import ViewSet
+
+from .serializers import UserSerializer
 
 
 class UserView(ViewSet):
-    pass
+    @action(methods=['post'], detail=False)
+    def singin(self, request, pk=None):
+        user_serializer = UserSerializer(data=request['data'])
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            login(request, user)
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=HTTP_200_OK)
+        else:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+    @action(methods=['post'], detail=False)
+    def singup(self, request, pk=None):
+        user_serializer = UserSerializer(data=request['data'])
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            login(request, user)
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=HTTP_201_CREATED)
+        else:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+    @action(methods=['post'], detail=False)
+    def signout(self, request, pk=None):
+        logout(request)
+        return Response(status=HTTP_200_OK)
